48c4679fe689112f9b62ccdef53d79bbc6eeb210
riyadomf <riyad.omf@gmail.com>
2023-05-02 19:11:54 +0600

    Add Todo page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,23 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todos = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(title: "...", body: "...")
+
+    if @todo.save
+      redirect_to @todo
+    else 
+      render :new, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "todos#index"
-  get "/todos", to: "todos#index"
+
+  resources :todos
 end
